aa3416a31fed84061d3828e634ccbe95c93c855b
KARTHIK D <karthikdhanapal03@gmail.com>
2026-06-17 20:12:17 +0530

    Create ECommerceSearchExample.java

---
+++ Week1/Algorithms_Data_Structures/ECommerceSearchExample/ECommerceSearchExample.java
@@ -0,0 +1,76 @@
+class Product {
+
+    int productId;
+    String productName;
+    String category;
+
+    public Product(int productId, String productName, String category) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+    }
+}
+
+public class ECommerceSearchExample {
+
+    // Linear Search
+    public static Product linearSearch(Product[] products, int id) {
+
+        for (Product product : products) {
+            if (product.productId == id) {
+                return product;
+            }
+        }
+
+        return null;
+    }
+
+    // Binary Search
+    public static Product binarySearch(Product[] products, int id) {
+
+        int left = 0;
+        int right = products.length - 1;
+
+        while (left <= right) {
+
+            int mid = (left + right) / 2;
+
+            if (products[mid].productId == id) {
+                return products[mid];
+            }
+
+            if (products[mid].productId < id) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        Product[] products = {
+                new Product(101, "Laptop", "Electronics"),
+                new Product(102, "Phone", "Electronics"),
+                new Product(103, "Shoes", "Fashion"),
+                new Product(104, "Watch", "Accessories"),
+                new Product(105, "Bag", "Fashion")
+        };
+
+        Product linearResult = linearSearch(products, 103);
+
+        if (linearResult != null) {
+            System.out.println("Linear Search Found: "
+                    + linearResult.productName);
+        }
+
+        Product binaryResult = binarySearch(products, 104);
+
+        if (binaryResult != null) {
+            System.out.println("Binary Search Found: "
+                    + binaryResult.productName);
+        }
+    }
+}
