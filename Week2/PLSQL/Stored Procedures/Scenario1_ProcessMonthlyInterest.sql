a5692da0664977e4274cc193a59736b11c287238
KARTHIK D <karthikdhanapal03@gmail.com>
2026-06-25 14:38:16 +0530

    Update Scenario1_ProcessMonthlyInterest.sql

--- Week2/PLSQL/Stored Procedures/Scenario1_ProcessMonthlyInterest.sql
+++ Week2/PLSQL/Stored Procedures/Scenario1_ProcessMonthlyInterest.sql
@@ -1,1 +1,16 @@
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest
+AS
+BEGIN
+UPDATE Accounts
+SET Balance = Balance + (Balance * 0.01)
+WHERE AccountType = 'SAVINGS';
+
+```
+COMMIT;
+
+DBMS_OUTPUT.PUT_LINE('Monthly interest processed successfully');
+```
+
+END;
+/
 
