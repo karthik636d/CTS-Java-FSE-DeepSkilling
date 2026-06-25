20d28e70e585ebfff9ecbd12647cef0b63e51d27
KARTHIK D <karthikdhanapal03@gmail.com>
2026-06-25 14:39:03 +0530

    Create Scenario2_UpdateEmployeeBonus.sql

---
+++ Week2/PLSQL/Stored Procedures/Scenario2_UpdateEmployeeBonus.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus(
+p_department IN VARCHAR2,
+p_bonus_percent IN NUMBER
+)
+AS
+BEGIN
+UPDATE Employees
+SET Salary = Salary + (Salary * p_bonus_percent / 100)
+WHERE Department = p_department;
+
+```
+COMMIT;
+
+DBMS_OUTPUT.PUT_LINE('Bonus updated for department: ' || p_department);
+```
+
+END;
+/
