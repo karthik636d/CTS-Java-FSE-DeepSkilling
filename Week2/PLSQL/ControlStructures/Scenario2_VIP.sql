2dc9b8f79a7bf2f2860cfe6b0ec8b51df049ccb7
KARTHIK D <karthikdhanapal03@gmail.com>
2026-06-25 14:36:00 +0530

    Update Scenario2_VIP.sql

--- Week2/PLSQL/ControlStructures/Scenario2_VIP.sql
+++ Week2/PLSQL/ControlStructures/Scenario2_VIP.sql
@@ -1,1 +1,24 @@
+DECLARE
+BEGIN
+FOR c IN (
+SELECT CustomerID
+FROM Customers
+WHERE Balance > 10000
+)
+LOOP
+UPDATE Customers
+SET IsVIP = 'TRUE'
+WHERE CustomerID = c.CustomerID;
+
+```
+    DBMS_OUTPUT.PUT_LINE(
+        'Customer ' || c.CustomerID || ' promoted to VIP'
+    );
+END LOOP;
+
+COMMIT;
+```
+
+END;
+/
 
